d4ffc61838919791aee90c7e491d1ef175e615bd
Alphaswagat <swagat.kakad@alpha-ict.in>
2024-03-04 12:43:51 +0530

    Initial Commit

---
+++ Hotel Lock/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hotel Lock
+//
+//  Created by Alpha-mac3 on 14/12/1945 Saka.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Hotel LockUITests/Hotel_LockUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Hotel_LockUITestsLaunchTests.swift
+//  Hotel LockUITests
+//
+//  Created by Alpha-mac3 on 14/12/1945 Saka.
+//
+
+import XCTest
+
+final class Hotel_LockUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
